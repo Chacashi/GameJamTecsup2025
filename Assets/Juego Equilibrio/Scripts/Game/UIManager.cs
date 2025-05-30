dc3e62c7b12f589f3859a14f3dde57cc9d8ee928
Chacashi <eljohndelapuna@gmail.com>
2025-05-29 21:39:41 -0500

    Add barTimePlayer

--- Assets/Juego Equilibrio/Scripts/Game/UIManager.cs
+++ Assets/Juego Equilibrio/Scripts/Game/UIManager.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,17 +8,21 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Slider barTime;
     [SerializeField] private Image statePlayer;
     [SerializeField] private float maxValuePlayer;
-
+    [SerializeField] private float valueNoise;
+    [SerializeField] private Sprite[] arrayStatesPlayer;
+    public static event Action OnPlayerDeathd;
 
 
     private void Start()
     {
         barTime.minValue = 0;
-       // barTime.maxValue = 
+        barTime.maxValue = maxValuePlayer;
+        barTime.value = maxValuePlayer/2;
     }
     private void Update()
     {
-        //barTime.value 
+        SubstractValue();
+        CalculateStatePlayer(barTime.value);
     }
     private void OnEnable()
     {
@@ -29,7 +35,40 @@ public class UIManager : MonoBehaviour
     }
     void IncrementBarTime()
     {
-        barTime.value++; 
+        barTime.value +=valueNoise; 
+    }
+
+    private void CalculateStatePlayer( float value)
+    {
+        if (value <= 0)
+        {
+            OnPlayerDeathd?.Invoke();
+        }
+        else if(value<5 && value > 0)
+        {
+            statePlayer.sprite = arrayStatesPlayer[0];
+        }
+        else if(value>=5 && value < 12)
+        {
+            statePlayer.sprite = arrayStatesPlayer[1];
+        }
+        else if(value>=12 && value < 20)
+        {
+            statePlayer.sprite = arrayStatesPlayer[2];
+        }
+        else
+        {
+            OnPlayerDeathd?.Invoke();
+        }
+
+    }
+
+    void SubstractValue()
+    {
+        if (barTime.value > barTime.minValue)
+        {
+            barTime.value -= Time.deltaTime;
+        }
     }
 
 
